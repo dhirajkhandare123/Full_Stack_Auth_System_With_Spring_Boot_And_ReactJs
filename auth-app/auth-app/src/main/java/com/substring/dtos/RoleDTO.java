1726bff776b0149c28fb38162a982a58b35cbfa6
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-11 15:41:35 +0530

    Create RoleDTO.java

---
+++ auth-app/auth-app/src/main/java/com/substring/dtos/RoleDTO.java
@@ -0,0 +1,21 @@
+package com.substring.dtos;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDTO {
+
+    private UUID id;
+    private String name;
+}
