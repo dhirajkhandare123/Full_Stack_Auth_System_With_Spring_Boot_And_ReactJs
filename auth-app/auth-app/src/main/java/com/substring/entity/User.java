c61e7b4c182cd4105c00f03d8b620496e70598ef
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-11 15:41:54 +0530

    Create User.java

---
+++ auth-app/auth-app/src/main/java/com/substring/entity/User.java
@@ -0,0 +1,53 @@
+package com.substring.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+    @Column(name = "user_email", unique = true, length = 300)
+    private String email;
+    private String name;
+    private String image;
+    private Boolean enable=true;
+    private Instant createdAt=Instant.now();
+    private Instant updatedAt=Instant.now();
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Roles> roles = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate(){
+        Instant now = Instant.now();
+        if(createdAt==null) createdAt=now;
+        updatedAt=now;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt=Instant.now();
+    }
+}
