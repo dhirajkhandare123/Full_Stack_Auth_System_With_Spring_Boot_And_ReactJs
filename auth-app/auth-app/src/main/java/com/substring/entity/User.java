2a4addae383b09d56f182eadd2981ba9a791d203
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-16 18:56:58 +0530

    Update User.java

--- auth-app/auth-app/src/main/java/com/substring/entity/User.java
+++ auth-app/auth-app/src/main/java/com/substring/entity/User.java
@@ -2,11 +2,12 @@ package com.substring.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Getter
 @Setter
@@ -15,7 +16,7 @@ import java.util.UUID;
 @Builder
 
 @Entity(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "user_id")
@@ -50,5 +51,39 @@ public class User {
     @PreUpdate
     protected void onUpdate(){
         updatedAt=Instant.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = roles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName())).toList();
+
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enable;
     }
 }
